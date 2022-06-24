f048a7ebe49e37925bdec1c83be0a78c58811f21
mweisglass <68035603+mweisglass@users.noreply.github.com>
2022-06-24 13:19:45 -0500

    New File

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Hi! My name is";
+let name = "Matthew";
+let raceName = "Great Race";
+let role = "spectator";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let raceLength = 3;
+let numRacers = 26;
+let year = 2023;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let merchandise = ["fan glove", "souvenir cup", "race t-shirt"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let isEnjoyable = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(`${greetings} ${name}.`);
+console.log(`I'm at the ${raceName} of ${year} as a ${role}.`);
+console.log(`The race will be about ${raceLength} hours long.`);
+console.log(`It's going to be a tough race since there are ${numRacers} racers!`);
+console.log(`I'm going to be waving my ${merchandise[0]} the entire time!`);
+console.log(`Do I enjoy it? ${isEnjoyable}.`);
+console.log(`Come experience the thrill!`);
